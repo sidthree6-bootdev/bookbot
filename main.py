b2fda34fb142eadbce6ede6f4cc7e2597fec73b9
Sidd <psiddhu001@gmail.com>
2025-08-14 13:09:50 -0400

    Refactor

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
+from stats import book_word_count
+
 def get_book_text(filepath):
     with open(filepath) as file_content:
         return file_content.read()
-
-def book_word_count(book_text):
-    words = book_text.split()
-    return len(words)
 
 def main():
     book_text = get_book_text('books/frankenstein.txt')
---
+++ stats.py
@@ -0,0 +1,3 @@
+def book_word_count(book_text):
+    words = book_text.split()
+    return len(words)
