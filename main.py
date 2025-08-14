ac8870ddb1928f049f716b463561ea92a9dc5f25
Sidd <psiddhu001@gmail.com>
2025-08-14 13:01:56 -0400

    Book word count added

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("greetings boots")
+def get_book_text(filepath):
+    with open(filepath) as file_content:
+        return file_content.read()
+
+def book_word_count(book_text):
+    words = book_text.split()
+    return len(words)
+
+def main():
+    book_text = get_book_text('books/frankenstein.txt')
+    book_word_count_value = book_word_count(book_text)
+    print(f"{book_word_count_value} words found in the document")
+
+main()
