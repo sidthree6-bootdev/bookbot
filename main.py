811729b68b517871655ab5d3b8fa7b451620697e
Sidd <psiddhu001@gmail.com>
2025-08-14 13:15:08 -0400

    Character count

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import book_word_count
+from stats import book_word_count, book_character_count
 
 def get_book_text(filepath):
     with open(filepath) as file_content:
@@ -8,5 +8,6 @@ def main():
     book_text = get_book_text('books/frankenstein.txt')
     book_word_count_value = book_word_count(book_text)
     print(f"{book_word_count_value} words found in the document")
+    print(book_character_count(book_text))
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,15 @@
 def book_word_count(book_text):
     words = book_text.split()
     return len(words)
+
+def book_character_count(book_text):
+    return_dict = {}
+
+    for char in book_text:
+        char = char.lower()
+        if char in return_dict:
+            return_dict[char] += 1
+        else:
+            return_dict[char] = 1
+
+    return return_dict
